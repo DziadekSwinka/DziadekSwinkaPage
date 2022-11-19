b93eaf0f25fe704f80a1822f7881bc4389b942f6
Jakub <113696735+DziadekSwinka@users.noreply.github.com>
2022-11-19 19:12:15 +0100

    19.11

--- dymek.js
+++ dymek.js
@@ -1,14 +1,9 @@
-const item1=document.getElementById('menu1');
-const item2=document.getElementById('menu2');
-const item3=document.getElementsByClassName('.menu3');
-
-if(item1){
-	alert("BBB");
-	item1.addEventListener("mouseover",function(){document.getElementById("dymek").style.display = "block";});
-}
-if(item2){
-	item2.addEventListener("mouseover",function(){document.getElementById("dymek").style.display = "block";});
-}
-//if(item3){
-	//item3.addEventListener("mouseover",function(){document.getElementById("dymek").style.display = "block";});
-//}
+function dymek(nazwa)
+{
+	document.getElementById("dymek").style.display="block";
+	document.getElementById("dymek").style.backgroundImage='url("Images//'+nazwa+'.png")';
+};
+function dymek_out()
+{
+	document.getElementById("dymek").style.display="none";
+};

3ccc4d0872110de74a2e9f8bd25e0a8312c693d9
Jakub <113696735+DziadekSwinka@users.noreply.github.com>
2022-11-20 22:17:00 +0100

    20.11

--- dymek.js
+++ dymek.js
@@ -1,7 +1,7 @@
 function dymek(numer)
 {
 	//document.getElementById("dymek"+numer).style.backgroundImage='url("Images//dziadek dymek '+numer+'.png")';
-	//$("#dymek"+numer).fadeIn(500);
+	//	$("#dymek"+numer).fadeIn(500);
 	document.getElementById("dymek"+numer).style.display="block";
 };
 function dymek_out(numer)
--- scripts.js
+++ scripts.js
@@ -4,3 +4,12 @@ window.addEventListener('scroll',function()
 	var domek=document.querySelector('#domek');
 	domek.style.transform='translateX('+scroll+'px)';
 });
+function wczytajGalerie()
+{
+	for(var i=1;i<9;i++)
+	{
+		$("#g"+i).style="background-image: url('Images/g"+i+".png');";
+		$("#g"+i).style="background-size: contain;";
+		$("#g"+i).style="background-repeat: no-repeat;";
+	}
+}
